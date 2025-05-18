0f0c7911e54c65ffc01262defb8e06d0e2d5912f
SonuKumarMgs <sonumgs132@gmail.com>
2025-05-18 22:13:29 +0530

    Test Cases

---
+++ NUnit_Test/AuthServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using NUnit.Framework;
+using Org.BouncyCastle.Crypto.Generators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserandDocumentManagement_JKT.Data;
+using UserandDocumentManagement_JKT.DTOs;
+using UserandDocumentManagement_JKT.Models;
+using UserandDocumentManagement_JKT.Sevices.Implementations;
+using UserandDocumentManagement_JKT.Sevices.Interfaces;
+
+namespace NUnit_Test
+{
+    [TestFixture]
+    public class AuthServiceTests
+    {
+        private Mock<ISignupService> _signupService;
+        private Mock<AppDbCotext> _context;
+        private List<UserDto> userdto;
+        private SignupServiceImpl signupServiceImpl;
+
+        [SetUp]
+        public void Setup()
+        {
+            _signupService = new Mock<ISignupService>();
+            userdto = new List<UserDto>();
+            userdto.Add(new UserDto() { Username = "TestCase", Email = "test@gmail.com", Password = "Test@12345" });
+        }
+
+        [Test]
+        public async Task AuthenticateAsync_ValidUser_ReturnsUser()
+        {
+            var userToReturn = userdto.FirstOrDefault(x => x.Username == "TestCase");
+            signupServiceImpl = new SignupServiceImpl(_context.Object);
+            var result = await signupServiceImpl.UserSingup(userToReturn);
+            Assert.IsNotNull(result);
+        }
+
+    }
+
+}
---
+++ NUnit_Test/UserManagementBL.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using NUnit.Framework;
+using Org.BouncyCastle.Crypto.Macs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserandDocumentManagement_JKT.Data;
+using UserandDocumentManagement_JKT.DTOs;
+using UserandDocumentManagement_JKT.Models;
+using UserandDocumentManagement_JKT.Sevices.Implementations;
+using UserandDocumentManagement_JKT.Sevices.Interfaces;
+
+namespace NUnit_Test
+{
+    [TestFixture]
+    public class UserManagementBL
+    {
+        private IUserService _userService;
+        private AppDbCotext _context;
+        private List<User> usersProfile;
+        private UserServiceImpl userServiceImpl;
+
+        [SetUp]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<AppDbCotext>()
+            .UseInMemoryDatabase(databaseName: "TestDb")
+            .Options;
+
+            _context = new AppDbCotext(options);
+            _context.Users.Add(new User
+            {
+                Id = Guid.NewGuid(),
+                Username = "rakeshkumar",
+                Password = "hashedpassword",
+                Email = "rkmgs@gmail.com",
+                Role = "editor"
+            });
+            _context.SaveChanges();
+
+            _userService = new UserServiceImpl(_context);
+
+            // _context = new Mock<AppDbCotext>();
+            //_userService = new Mock<IUserService>();
+            //usersProfile = new List<User>();
+            //usersProfile.Add(new User() {  Username = "rakeshkumar", Email = "rkmgs@gmail.com", Password = "AQAAAAIAAYagAAAAEHgd4Bc/ro1Qj0Wm3z0ON60Q5cHctpeQ9beKFXw9FVhSM09alZHGKapDBqVy8Fp6Pg==", Role= "editor" });
+            var userDbSet = GetQueryableMockDbSet(usersProfile);
+
+            //_context.Setup(c => c.Users).Returns(userDbSet.Object);
+        }
+
+        [Test]
+        public async Task GetAllValidUser_ReturnsUser()
+        {
+           // userServiceImpl = new UserServiceImpl(_context.);
+            var result = await userServiceImpl.GetAllUsersAsync();
+            Assert.IsNotNull(result);
+        }
+        [Test]
+        public async Task GetUsersByUserId_ReturnsUser()
+        {
+            var userId = Guid.Parse("0196caac-9063-73e5-aa4d-d500033361da");
+           // userServiceImpl = new UserServiceImpl(_context.Object);
+            var result = await userServiceImpl.GetUsersByUserIdAsync(userId);
+            Assert.IsNotNull(result);
+        }
+        [Test]
+        public async Task UpdateUserRoleById_ReturnsUser()
+        {
+            var userId = Guid.Parse("0196caac-9063-73e5-aa4d-d500033361da");
+            string newRole = "viewer";
+           // userServiceImpl = new UserServiceImpl(_context.Object);
+            var result = await userServiceImpl.UpdateUserRoleByIdAsync(userId, newRole);
+            Assert.IsNotNull(result);
+        }
+
+        public static Mock<DbSet<T>> GetQueryableMockDbSet<T>(List<T> sourceList) where T : class
+        {
+            var queryable = sourceList.AsQueryable();
+            var dbSet = new Mock<DbSet<T>>();
+
+            dbSet.As<IQueryable<T>>().Setup(m => m.Provider).Returns(queryable.Provider);
+            dbSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(queryable.Expression);
+            dbSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(queryable.ElementType);
+            dbSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(queryable.GetEnumerator());
+
+            dbSet.Setup(d => d.Add(It.IsAny<T>())).Callback<T>(sourceList.Add);
+
+            return dbSet;
+        }
+
+    }
+}
